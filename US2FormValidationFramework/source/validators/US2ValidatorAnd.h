a3bad6afefe4b84f2a69f8166d522dfeb7dab683
Matthew Purland <m.purland@gmail.com>
2012-10-10 19:44:32 -0700

    Added validators for Or/And.

---
+++ US2FormValidationFramework/source/validators/US2ValidatorAnd.h
@@ -0,0 +1,14 @@
+//
+//  US2ValidatorAnd.h
+//  US2FormValidationFramework
+//
+//  Created by Matthew Purland on 10/10/12.
+//  Copyright (c) 2012 ustwo™. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "US2ValidatorComposite.h"
+
+@interface US2ValidatorAnd : US2ValidatorComposite
+
+@end
